3fd82c4f8effd3cf4145cae999343fd72084db15
Nicklas <nicklas@estatetool.dk>
2026-04-26 17:29:27 +0200

    feat(Footer): add Instagram link and update contact information

--- src/app/shared/icons.ts
+++ src/app/shared/icons.ts
@@ -1,7 +1,8 @@
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faBuilding, faPhone, faEnvelope, faBars, faInfo, faHouse, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 export function registerIcons(library: FaIconLibrary): void {
-  library.addIcons(faBuilding, faPhone, faEnvelope, faBars, faInfo, faCircleQuestion, faHouse, faAngleRight);
+  library.addIcons(faBuilding, faPhone, faEnvelope, faBars, faInfo, faCircleQuestion, faHouse, faAngleRight, faInstagram);
 }
